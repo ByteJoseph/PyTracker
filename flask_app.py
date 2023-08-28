ef80b4223cf5f5df8687cf2a872d000ce220bc8b
Joseph Joseph <131424720+its-joseph-230606@users.noreply.github.com>
2023-08-28 16:20:34 +0530

    Add files via upload

--- flask_app.py
+++ flask_app.py
@@ -1,1 +1,14 @@
-no
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+@app.route("/")
+def home():
+	return  render_template("home.html")
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
+@app.route("/<ip>")
+def search(ip):
+   print(ip)
+   return render_template("search.html", prompt=ip)
+app.run()
